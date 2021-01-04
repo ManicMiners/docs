3b56bba879aad6f5b84d5b1debaeccbc7e17554a
Akorax <ernst_hultqvist@hotmail.com>
2021-01-04 09:38:17 +0100

    More highlighting

--- docsify.js
+++ docsify.js
@@ -4154,7 +4154,7 @@
 				greedy: !0,
 			},
 		  ],
-		  variable: /\b(?:when|if)\b/,
+		  variable: /\b(?:when|if|comments|info|tiles|height|resources|objectives|buildings|landslidefrequency|lavaspread|miners|briefing|vehicles|creatures)\b/,
 		  constant: /\b(?:crystals|ore|studs|air|miners|vehicles|buildings|time|lastminer|lastvehicle|lastbuilding|ConstructedBuilding|datafield|random|get)\b/, <!-- macros -->
 		  atrule: /\b(?:int|string|boolean|float|miner|vehicle|building|arrow)\s/,
           boolean: /\b(?:true|false)\b/,
@@ -4164,7 +4164,7 @@
 				greedy: !0,
 			},
 		  ],
-		  keyword: /\b(?:init|reset|pause|unpause|resume|resetspeed|miners|vehicles|buildings|light|lights)|(?:drill|msg|place|wait|truewait|win|lose|sound|pan|shake|save|speed|resetspeed|showarrow|highlight|highlightarrow|hidearrow)(?=:)\b/,
+		  keyword: /(\b(?:init|reset|pause|unpause|resume|resetspeed|miners|vehicles|buildings|light|lights)|(?:drill|msg|place|wait|truewait|win|lose|sound|pan|shake|save|speed|resetspeed|showarrow|highlight|highlightarrow|hidearrow)(?=:)\b)/,
           number: /\b0x[\da-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
           operator: /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
           punctuation: /[(){};:,\[\]]/,
